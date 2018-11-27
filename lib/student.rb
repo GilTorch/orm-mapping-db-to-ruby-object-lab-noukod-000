7bca473dd6fcab58bfeab1e68359b7ce40f2be1e
Gilbert Torchon <gilbert@noukod.com>
2018-11-27 17:52:20 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,10 +32,11 @@ class Student
 
   def self.students_below_12th_grade
     all_students=DB[:conn].execute("SELECT * FROM students WHERE grade <= 11")
+      binding.pry
     all_students.map do |row|
       Student.new_from_db(row)
     end
-    binding.pry
+  
     all_students
   end
 
