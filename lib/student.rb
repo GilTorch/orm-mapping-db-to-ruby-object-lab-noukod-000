f887df69d0d08e6edf3d03e839ad0a7168e79e44
Gilbert Torchon <gilbert@noukod.com>
2018-11-27 17:49:20 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,6 +22,15 @@ class Student
     all_students
   end
 
+  def self.all_students_in_grade_9 
+    all_students=DB[:conn].execute("SELECT * FROM students WHERE grade=9")
+    all_students.map do |row|
+      Student.new_from_db(row)
+    end
+    all_students
+  end
+  
+
   def self.find_by_name(name)
     # find the student in the database given a name
     # return a new instance of the Student class
